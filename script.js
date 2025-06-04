00821c3a3ac8af6b1958814d29881cb3264071d1
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2025-06-05 01:05:20 +0530

    first commit for pet gallery

---
+++ script.js
@@ -0,0 +1,64 @@
+var text = document.getElementById("title");
+
+document.getElementsByClassName("box1")[0].addEventListener("mouseover", () => {
+    text.textContent = "Cat and Dog!🤩";
+});
+document.getElementsByClassName("box1")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box2")[0].addEventListener("mouseover", () => {
+    text.textContent = "Hedgehog🦔";
+});
+document.getElementsByClassName("box2")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box3")[0].addEventListener("mouseover", () => {
+    text.textContent = "Parrot🦜";
+});
+document.getElementsByClassName("box3")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box4")[0].addEventListener("mouseover", () => {
+    text.textContent = "Foal🐎";
+});
+document.getElementsByClassName("box4")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box5")[0].addEventListener("mouseover", () => {
+    text.textContent = "Baby Turtle🐢";
+});
+document.getElementsByClassName("box5")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box6")[0].addEventListener("mouseover", () => {
+    text.textContent = "Kittens😍🐱";
+});
+document.getElementsByClassName("box6")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box7")[0].addEventListener("mouseover", () => {
+    text.textContent = "Corgi🐶😂";
+});
+document.getElementsByClassName("box7")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box8")[0].addEventListener("mouseover", () => {
+    text.textContent = "Python🐍❤️";
+});
+document.getElementsByClassName("box8")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
+
+document.getElementsByClassName("box9")[0].addEventListener("mouseover", () => {
+    text.textContent = "Fish🐠";
+});
+document.getElementsByClassName("box9")[0].addEventListener("mouseout", () => {
+    text.textContent = "Pet Gallery❣️";
+});
